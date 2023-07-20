d5e2243ca3ef0ef30339fae7d53efd071fb8babf
65050235 <65050235@kmitl.ac.th>
2023-07-20 20:47:33 +0700

    lab3q4

---
+++ lab3q4_65235.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+	int a = 0;
+	int sum = 0;
+	float count = 0;
+	printf("enter a number : ");
+        scanf("%d",&a);
+	sum += a;
+	
+	while(a > 0){
+		count++;
+		printf("enter a number : ");
+        	scanf("%d",&a);
+		sum += a;
+	}
+	printf("sum = %d\n",sum);
+	printf("avg = %d\n",(sum/count));
+}
+
